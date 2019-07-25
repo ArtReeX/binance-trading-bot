bd8d3222661e6f11631b23b8ccecd2dbc4d15cd1
Arthur Lazarenko <Arthur.Lazarenko@icloud.com>
2019-07-26 02:04:53 +0300

    Правка информации о ордере продажи.

--- tracking/main.go
+++ tracking/main.go
@@ -125,7 +125,7 @@ func DirectionTracking(direction Direction,
 						}
 
 						log.Println("Создан ордер", order.OrderID, "на продажу с направлением", orderInfo.BuyOrder.Symbol, "периодом",
-							direction.Interval, "по цене", orderInfo.BuyOrder.Price, "и количеством",
+							direction.Interval, "по цене", currentPrice, "и количеством",
 							orderInfo.BuyOrder.OrigQuantity, "выгода составит", currentPrice*quantity-purchasePrice*quantity, direction.Quote)
 
 						orderInfo.BuyOrder = nil
